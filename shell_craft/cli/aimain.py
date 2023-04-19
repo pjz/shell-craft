f08caf61c6bd34f27b5e10d9337071fd859a0399
Paul Jimenez <pj@place.org>
2023-04-19 00:21:28 -0400

    add refactor/document/test options

--- shell_craft/cli/aimain.py
+++ shell_craft/cli/aimain.py
@@ -1,6 +1,7 @@
 import sys
 
 import click
+from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup
 
 from .key import get_api_key
 from .prompt import get_calling_shell, prompt_choices
@@ -23,9 +24,15 @@ PROMPT_DEFAULT = get_calling_shell()
 @ai.command()
 @click.argument("query", nargs=-1)
 @click.option('--prompt', default=PROMPT_DEFAULT, type=PROMPT_CHOICE, help='The prompt to use')
-@click.option('--model', default="gpt-3.5-turbo", help='openai model to use')
+@click.option('--model', default="gpt-3.5-turbo",
+              help='openai model to use; see https://platform.openai.com/docs/models for options')
+@optgroup.group('Request type', cls=RequiredMutuallyExclusiveOptionGroup,
+                help='The kind of request to make')
+@optgroup.option('--refactor', is_flag=True, default=False, help='Refactor the code')
+@optgroup.option('--document', is_flag=True, default=False, help='Documnet the code')
+@optgroup.option('--test', is_flag=True, default=False, help='Test the code')
 @click.pass_context
-def ask(ctx, query, prompt, model):
+def ask(ctx, query, prompt, model, refactor, document, test):
     """
     Ask the AI something either on the command line, or if that's empty then read from stdin.
     """
@@ -36,10 +43,18 @@ def ask(ctx, query, prompt, model):
 
     api_key = ctx.obj.get('api_key') or get_api_key()
 
+    prompt = PromptFactory.get_prompt(prompt)
+    if refactor:
+        prompt = prompt.refactoring
+    elif document:
+        prompt = prompt.documentation
+    elif test:
+        prompt = prompt.testing
+
     print(
         Service(
             api_key=api_key,
-            prompt=PromptFactory.get_prompt(prompt),
+            prompt=prompt,
             model=model
         ).query(
             message=' '.join(query)
